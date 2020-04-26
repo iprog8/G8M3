07ee43df5d97f861aa62ba54dbf085c658660541
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-04-26 19:58:13 +0300

    Tema L6 partea a doua

--- Teme/Gabi/L6/bazaDeDateIpro.sql
+++ Teme/Gabi/L6/bazaDeDateIpro.sql
@@ -4,6 +4,7 @@ go
 use ipro 
 go
 
+--prima parte
 --In baza de date IPRO creati tabela Cursant cu campurile: Id, Nume, DataNastere, Localitate, Tara;
 create table Cursant
 (
@@ -121,19 +122,98 @@ go
 insert into CursantGrupa
 (CursantId,GrupaId,NotaFinala,Continua)
 values
-('10','2','10','True'),
-('10','3','7','True'),
-('10','4','8',''),
-('5','2','10','True'),
-('5','3','9','True'),
-('5','4','7',''),
-('7','2','10','True'),
-('7','3','8','True'),
-('7','4','7',''),
-('11','2','10','True'),
-('11','3','9','True'),
-('11','4','7',''),
-('20','2','10','True'),
-('20','3','8','True'),
-('20','4','9','')
+('10','1','10','True'),
+('10','2','7','True'),
+('10','3','8',''),
+('5','1','10','True'),
+('5','2','9','True'),
+('5','3','7',''),
+('7','1','10','True'),
+('7','2','8','True'),
+('7','3','7',''),
+('11','1','10','True'),
+('11','2','9','True'),
+('11','3','7',''),
+('20','1','10','True'),
+('20','2','8','True'),
+('20','3','9','')
+go
+
+--Partea a doua
+--Selectati toate grupele de modulul 1; Hint: inner join
+select *
+from Grupa G inner join Modul M on G.ModulId = M.Id
+where G.Denumire like '%M1%';
+go
+
+--Selectati toti cursantii de la grupa G8M2; Hint: inner join	
+select *
+from Cursant C inner join CursantGrupa Cg on C.Id = Cg.CursantId 
+where Cg.GrupaId = 2;
+go
+
+--Selectati Modulele cu care nu s-a facut nici o grupa pana acum; Hint: left join
+select *
+from Modul M left join Grupa G on M.Id = G.ModulId
+where G.ModulId is null;
+go
+
+--Selectati toti cursantii care nu au participat la nici o grupa; Hint: right join
+select *
+from Cursant C left join CursantGrupa Cg on C.Id = Cg.CursantId 
+where Cg.CursantId is null;
+go
+
+--Selectati toti cursantii care au participat la M1; Hint: 3 inner join
+select *
+from Cursant C left join CursantGrupa Cg on C.Id = Cg.CursantId
+inner join Grupa G on G.Id = Cg.GrupaId
+inner join Modul M on M.Id = G.ModulId
+where M.Denumire = 'M1';
+go
+
+--Selectati toti cursantii;
+select *
+from Cursant
+go
+
+--Selectati numarul total al cursantilor; Hint: agregare
+select 
+	count(*) NumarulCursantilor 
+from Cursant
+go
+
+--Selectati toti cursantii din Romania;
+select *
+from Cursant C
+where C.Tara = 'Romania';
+go
+
+--Selectati numarul cursantilor din Romania; Hint: agregare + grupare
+select
+	Tara,
+	count(Tara) as NumarCursantiRomania
+from Cursant C
+group by Tara
+go
+
+--Selectati toti cursantii a caror nota este mai mare de 7;
+select *
+from Cursant C inner join CursantGrupa CG on C.Id = Cg.CursantId
+where Cg.NotaFinala > 7;
+go
+
+--Calculati media notelor tuturor cursantilor; Hint: agregare
+select 
+	avg(NotaFinala) as MedieNoteCursanti
+from CursantGrupa
+go
+
+--Calculati media notelor pe fiecare cursant; Hint: agregare + grupare
+select 
+	C.Nume,
+	Cg.CursantId,
+	avg(Cg.NotaFinala) as MedieNotePerCursant
+from CursantGrupa Cg inner join Cursant C on C.Id = Cg.CursantId
+group by Cg.CursantId, C.Nume
 go
