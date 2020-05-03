105a951534e3aaf65baee158ff0c1dc7aaa0b4e8
avramcristian <whiskyman_cv@yahoo.com>
2020-05-03 23:09:34 +0300

    Tema C# Companie si lista

---
+++ Laboratoare/Teme rezolvate/lab C/PrimaAplicatie/PrimaAplicatie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calcule;
+
+namespace PrimaAplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Neata, C#");
+            
+            Adunare add = new Adunare();
+            int suma = add.Calculeaza(5, 7);
+            Console.WriteLine(suma);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Teme/Avram Cristian/L8/Companie/Companie/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companie
+{
+    class Program
+    {
+        string numeCompanie;
+        ushort numarAngajati;
+        string tipCompanie;
+        DateTime dataInfiintarii;
+        string CUI;
+        string nrTelefon;
+        string localitate;
+        string tara;
+        bool areTVA;
+        ulong cifraDeAfaceri;
+        ulong datorii;
+        ulong incasari;
+
+        static void Main(string[] args)
+        {
+            Afiseaza();
+            AfiseazaPare();     
+        }
+
+        static void Afiseaza()
+        {
+            List<string> Angajati = new List<string>();
+            Angajati.Add("Popescu");
+            Angajati.Add("Ionescu");
+            Angajati.Add("Marinescu");
+
+             for (int i = 0; i < Angajati.Count; i++)
+            {
+                Console.WriteLine(Angajati[i]);
+            }
+
+        }
+        static void AfiseazaPare()
+        {
+            List<string> Angajati = new List<string>();
+            Angajati.Add("Popescu");
+            Angajati.Add("Ionescu");
+            Angajati.Add("Marinescu");
+
+
+
+            for (int i = 0; i < Angajati.Count; i++)
+            {
+                if (i % 2 ==0)
+                Console.WriteLine(Angajati[i]);
+            }
+
+        }
+    }
+}
