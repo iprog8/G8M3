d4767ec7e9521bfe7218bdd7117f5b78370d6ccd
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-05-03 16:11:21 +0300

    Lab 7 C#  PrimaAplicatie si Calcule

---
+++ Teme/Bogdan/C#/L7/PrimaAplicatie/PrimaAplicatie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaAplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Neata C#!");
+            Console.WriteLine("Neata C#!");
+            Avion();
+            Console.ReadKey();
+        }
+
+        static void Avion()
+        {
+            Console.WriteLine("Neata din Avion!!!");
+        }
+    }
+}
