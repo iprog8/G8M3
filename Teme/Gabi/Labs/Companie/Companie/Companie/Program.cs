c77e18d93ddd1a6bccfa6f347a229357bdb9c979
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-05-04 19:33:17 +0300

    Tema L9

---
+++ Teme/Gabi/Labs/Companie/Companie/Companie/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companie
+{
+    class Program
+    {
+        string numeCompanie;
+        int numarAngajati;
+        string tipCompanie;
+        DateTime dataInfiintarii = new DateTime(2004, 1, 4);
+        ushort CUI;
+        string numarTelefon;
+        string localitate;
+        string tara;
+        bool areTVA;
+        ulong cifraDeAfaceri;
+        int datorii;
+        ulong incasari;
+        static void Main(string[] args)
+        {
+            List<string> angajati = new List<string>();
+            angajati.Add("Hanu Gabriel");
+            angajati.Add("Avram Cristian");
+            angajati.Add("Bogdan Geagu");
+            angajati.Add("Vlad Octavian");
+            angajati.Add("Letitia Provian");
+            //afisare toti angajatii
+            for ( int i = 0; i < angajati.Count; i++)
+            {
+                Console.WriteLine(angajati[i]);
+            }
+            for ( int i = 0; i < angajati.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(angajati[i]);
+                }
+            }
+        }
+    }
+}
