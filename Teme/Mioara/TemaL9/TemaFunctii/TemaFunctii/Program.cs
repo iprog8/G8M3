c3d43fad799cd0476a1fde2bbb8ff952b23b9ad9
mioaraipro <stratmioara@yahoo.com>
2020-05-07 18:35:28 +0300

    Tema L 9 varianta  eu

---
+++ Teme/Mioara/TemaL9/TemaFunctii/TemaFunctii/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaFunctii
+{
+    class Progra
+    {
+   
+       
+        static void Main(string[] args)
+        {
+            long profitCalculat = CalculeazaProfit();
+            
+            Console.WriteLine($"Profitul calculat pentru  {valoareIncasari} si {costuri}  este {profitCalculat}.");
+            
+        }
+
+        static long CalculeazaProfit()
+        {
+            long valoareIncasari = 257;
+            long costuri = 189;
+            long profitCalculat = valoareIncasari - costuri;
+
+            return profitCalculat;
+        }
+
+        
+
+
+
+    }
+}
