ebb0f6ac1a2dd24a4948744f8d1618d159d37473
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-05-07 19:17:32 +0300

    tema test

--- Teme/Gabi/Labs/Companie/Companie/Companie/Program.cs
+++ Teme/Gabi/Labs/Companie/Companie/Companie/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,7 @@ namespace Companie
 {
     class Program
     {
+        public static Random rnd = new Random(); 
         string numeCompanie;
         int numarAngajati;
         string tipCompanie;
@@ -19,27 +22,71 @@ namespace Companie
         bool areTVA;
         ulong cifraDeAfaceri;
         int datorii;
-        ulong incasari;
-        static List<string> angajati = new List<string>();
+        int incasari;
+        public static List<string> angajati = new List<string>()
+        {
+            "Hanu Gabriel",
+            "Avram Cristian",
+            "Bogdan Geagu",
+            "Vlad Octavian",
+            "Letitia Provian"
+        };
         static void Main(string[] args)
         {
-            angajati.Add("Hanu Gabriel");
-            angajati.Add("Avram Cristian");
-            angajati.Add("Bogdan Geagu");
-            angajati.Add("Vlad Octavian");
-            angajati.Add("Letitia Provian");
-            //afisare toti angajatii
+            Afiseaza();
+            AfiseazaPare();
+            Console.WriteLine("");
+            //tema 2
+            int profit = CalculeazaProfit();
+            Console.WriteLine($"Profitul companiei este {profit} dolari");
+            int pierderi = CalculeazaPierderi();
+            Console.WriteLine($"Pierderile companiei sunt {pierderi} dolari");
+            Incaseaza(200);
+            int datoriiRamase = Plateste(413);
+            Console.WriteLine($"Compania mai are {datoriiRamase} datorii ramase");
+            Console.ReadKey();
+        }
+        static void Afiseaza()
+        {
+            for(int i = 0; i < angajati.Count; i++)
+            {
+                Console.WriteLine(angajati[i]);
+            }
+        }
+        static void AfiseazaPare()
+        {
+            Console.WriteLine("Angajatii cu pozitii pare sunt: ");
             for (int i = 0; i < angajati.Count; i++)
             {
                 Console.WriteLine(angajati[i]);
             }
-            for (int i = 0; i < angajati.Count; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(angajati[i]);
-                }
-            }
+        }
+        static int CalculeazaProfit()
+        {
+            int incasari = rnd.Next(1, 100);
+            int pierderi = rnd.Next(1, 100);
+            incasari -= pierderi;
+            return incasari;
+        }
+        static int CalculeazaPierderi()
+        {
+            int profit = rnd.Next(1, 100);
+            int pierderi = rnd.Next(-100, 1);
+            pierderi += profit;
+            return pierderi;
+        }
+        static void Incaseaza(int adunari)
+        {
+            int incasari = rnd.Next(1, 100);
+            incasari += adunari;
+            Console.WriteLine($"Incasarile companiei sunt {incasari} dolari");
+        }
+        static int Plateste(int plata)
+        {
+            int datorii = rnd.Next(1, 1000);
+            datorii -= plata;
+            if (plata > datorii) datorii = 0;
+            return datorii;
         }
     }
 }
