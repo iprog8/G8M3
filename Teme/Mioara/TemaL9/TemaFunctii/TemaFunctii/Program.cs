97e9a93ab93de15285e0a8dabebecda25f83c70d
mioaraipro <stratmioara@yahoo.com>
2020-05-07 19:37:17 +0300

    Tema L 9

--- Teme/Mioara/TemaL9/TemaFunctii/TemaFunctii/Program.cs
+++ Teme/Mioara/TemaL9/TemaFunctii/TemaFunctii/Program.cs
@@ -6,28 +6,67 @@ using System.Threading.Tasks;
 
 namespace TemaFunctii
 {
-    class Progra
+    class Program
     {
-   
-       
+        long valoareIncasari = 257;
+        long costuri = 189;
+
+
         static void Main(string[] args)
         {
             long profitCalculat = CalculeazaProfit();
             
             Console.WriteLine($"Profitul calculat pentru  {valoareIncasari} si {costuri}  este {profitCalculat}.");
+
+            long pierderi = CalculeazaPierderi();
+
+            Console.WriteLine($"Pierderile calculate pentru  {valoareIncasari} si {costuri}  sunt  {pierderi}.");
+
+            Incaseaza(83);
+
+            long datoriiRamase = Plateste(93);
             
+
         }
 
         static long CalculeazaProfit()
         {
-            long valoareIncasari = 257;
-            long costuri = 189;
+            
             long profitCalculat = valoareIncasari - costuri;
 
             return profitCalculat;
         }
 
-        
+        static long CalculeazaPierderi()
+
+        {
+            long pierderi = costuri - valoareIncasari;
+            
+            return pierderi;
+        }
+
+        static void SalveazaDataNasterii(DateTime dataNastere)
+        {
+            dataNasterii = dataNastere;
+        }
+
+        static void Incaseaza(long sumaIncasata)
+        {
+            long sumaIncasata = Incaseaza(sumaIncasata);
+            Console.WriteLine($"Suma incasata este {sumaIncasata}");
+        }
+
+        static long Plateste(long valoareFactura)
+        {
+            long datorii = 557;
+            long datoriiRamase = datorii - valoareFactura;
+            
+            return datoriiRamase;
+        }
+
+
+
+
 
 
 
