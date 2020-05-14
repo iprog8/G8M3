3c18af3c9b84ceb9b70daa5b0e635b6bf3d62fa3
avramcristian <whiskyman_cv@yahoo.com>
2020-05-14 16:09:47 +0300

    Parcarea

---
+++ Teme/Avram Cristian/L11/Parcare/Parcare/LocParcare.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcare
+{
+    public class LocParcare
+    {
+        public string CuloareArie { get; set; }
+        public string LiteraRand { get; set; }
+        public int Pozitie { get; set; }
+        public string TipLoc { get; set; }
+
+        public bool OcupatDeMasina(string Numar)
+        {
+            Console.WriteLine($"Parcela {LiteraRand} {Pozitie} {CuloareArie}, de tipul {TipLoc} este ocupata de masina cu numarul {Numar}");
+            return false;
+        }
+
+
+    }
+}
---
+++ Teme/Avram Cristian/L11/Parcare/Parcare/Masina.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcare
+{
+    public class Masina
+    {
+        public string Marca { get; set;}
+        public string Culoare { get; set;}
+        public string Tip { get; set; }
+        public string Numar { get; set; }
+
+        public void IntraInParcare()
+        {
+            Console.WriteLine($"Masina cu numarul {Numar}, tipul {Tip}, culoarea {Culoare}, marca {Marca} intra in parcare.");
+        }
+        
+        public void OcupaLoc(string LiteraRand, int Pozitie, string CuloareArie, string TipLoc)
+        {
+            Console.WriteLine($"Masina cu numarul {Numar} ocupa parcela {LiteraRand} {Pozitie} {CuloareArie}, de tipul {TipLoc}");            
+        }
+
+        public void ElibereazaLoc(string LiteraRand, int Pozitie, string CuloareArie, string TipLoc)
+        {
+            Console.WriteLine($"Masina cu numarul {Numar} elibereaza parcela {LiteraRand} {Pozitie} {CuloareArie}, de tipul {TipLoc}");
+        }
+
+        public void IeseDinParcare()
+        {
+            Console.WriteLine($"Masina cu numarul {Numar} , tipul {Tip}, culoarea {Culoare}, marca {Marca}  iese din parcare.");
+        }
+
+    }
+}
