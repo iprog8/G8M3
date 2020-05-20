5fc500255a475aaf9d93af0f12ba1b8cfd134f81
Ovidiu Rudi <djvidov@yahoo.com>
2020-05-20 12:46:34 +0300

    am lucrat la laboratorul 13

---
+++ Laboratoare/L13/ProprietatiSiFunctiiPart2/ProprietatiSiFunctiiPart2/Masina.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProprietatiSiFunctiiPart2
+{
+    public class Masina
+    {
+        public Masina()
+        {
+            AmCreatOMasina();
+            Culoare = "Alb";
+            Marca = "Dacia";
+            KmBord = 550;
+        }
+
+        public string Culoare { get; set; }
+        public string Marca { get; set; }
+        public int KmBord { get; set; }
+
+        public void SeDeplaseaza(int km)
+        {
+            KmBord += km;
+        }
+
+        public static void Servisare()
+        {
+
+        }
+
+        public static int NumarMasini { get; set; }
+
+        public static void AmCreatOMasina()
+        {
+            NumarMasini++;
+            Console.WriteLine($"Avem {NumarMasini} de masini create");
+        }
+    }
+}
---
+++ Laboratoare/L13/ProprietatiSiFunctiiPart2/ProprietatiSiFunctiiPart2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProprietatiSiFunctiiPart2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            
+            Masina focus = new Masina();
+            new Masina();
+            focus = new Masina();
+
+            if (focus != null)
+            {
+                focus.Culoare = "Rosu";
+                focus.Marca = "Ford Focus";
+                focus.SeDeplaseaza(100);
+            }
+
+            
+            Masina mondeo = new Masina();
+            mondeo.Culoare = "Rosu";
+            mondeo.Marca = "Ford MOndeo";
+            mondeo.SeDeplaseaza(100);
+            
+        }
+    }
+}
