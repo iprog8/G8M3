d2be9c564adbe7244efd95fc0bdef9147b58332b
Ovidiu Rudi <djvidov@yahoo.com>
2020-06-06 09:37:29 +0300

    Proiect initial pentru WoC

---
+++ WoC/Banca/Banca/Program.cs
@@ -0,0 +1,39 @@
+﻿using BazaDate;
+using log4net;
+using log4net.Config;
+using LogicaBanca;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banca
+{
+    static class Program
+    {
+        static ILog log;
+
+        static Program()
+        {
+            XmlConfigurator.Configure();
+            log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            log.Info($"App Banca started {Environment.UserDomainName}.{Environment.UserName}");
+        }
+
+        static void Main(string[] args)
+        {
+            //TODO:
+            // nuget package
+            // ref si out
+            // setup TFS
+            Date db = Date.LoadData<Date>(nameof(Date));
+
+
+
+
+            log.Info($"Main: App Banca stopped {Environment.UserDomainName}.{Environment.UserName}");
+        }
+    }
+}
---
+++ WoC/Banca/BazaDate/Database.cs
@@ -0,0 +1,61 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazaDate
+{
+    public class Database
+    {
+        private static ILog log;
+        static ILog Log
+        {
+            get
+            {
+                if (log == null)
+                {
+                    log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+                }
+                return log;
+            }
+        }
+
+        public Database(string file)
+        {
+            ConnectionString = file;
+        }
+
+        public string ConnectionString { get; internal set; }
+
+        public static T LoadData<T>(string file) where T : Database, new()
+        {
+            string fileData = null;
+            if (File.Exists(file))
+            {
+                fileData = File.ReadAllText(file);
+            }
+            if (fileData != null)
+            {
+                Log.Info($"LoadData: Baza de date a fost incarcata.");
+                return JsonConvert.DeserializeObject<T>(fileData);
+            }
+            else
+            {
+                Log.Info($"LoadData: Baza de date a fost Creata.");
+                return new T();
+            }
+        }
+
+        public void SaveData()
+        {
+            string fileData = JsonConvert.SerializeObject(this);
+            File.WriteAllText(ConnectionString, fileData);
+            Log.Info($"SaveData: Baza de date a fost salvata in: {ConnectionString}.");
+        }
+    }
+}
